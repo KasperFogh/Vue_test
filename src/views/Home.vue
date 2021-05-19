ce9e2bfc73f92eb048250ac655211992ca6f2935
Abdul-Rahman Abdurrohman <abdulbarakeh98@gmail.com>
2021-05-20 00:09:51 +0200

    add api to source control

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+  
+  <div class="d-flex flex-row">
+        <Login class="p-2"/>
+    <Register class="p-2"/>
+  </div>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Login from '@/components/Login.vue'
+import Register from '@/components/Register.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Login,
+    Register
+  }
+}
+</script>
